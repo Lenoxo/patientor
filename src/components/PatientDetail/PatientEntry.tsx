863414a514cc1d0e6e65fbb0de26c303cf969339
Emanuel Padilla <emanuehl159@gmail.com>
2024-05-10 18:40:09 -0500

    Add diagnosis name rendering with diagnosis code in PatientEntry

--- src/components/PatientDetail/PatientEntry.tsx
+++ src/components/PatientDetail/PatientEntry.tsx
@@ -1,14 +1,51 @@
-import { BaseEntry } from "../../types";
+import { useEffect, useState } from "react";
+import { BaseEntry, Diagnosis } from "../../types";
+import diagnosesService from "../../services/diagnoses";
 
 // I omit these properties because for now, they're not used in this component.
 type PatientBaseEntry = Omit<BaseEntry, "id" | "specialist">
 
 export default function PatientEntry({ date, description, diagnosisCodes }: PatientBaseEntry) {
+  const [patientDiagnosisInfo, setPatientDiagnosisInfo] = useState<Diagnosis[]>([])
+
+  function filterPatientDiagnosisInfo(listOfDiagnosis: Diagnosis[], patientDiagnosisCode: Diagnosis["code"]) {
+    const filteredDiagnosis = listOfDiagnosis.find((diagnosis) => {
+      return diagnosis.code === patientDiagnosisCode
+    })
+    return filteredDiagnosis as Diagnosis
+  }
+
+  useEffect(() => {
+    async function getDiagnosesInfo() {
+      const data = await diagnosesService.getAllDiagnoses()
+      if (!data) {
+        return console.error("diagnoses in PatientEntry is undefined");
+      }
+
+      if (!diagnosisCodes) {
+        return;
+      }
+
+      // This is a n sqrt operation, that's the reason why I left the validation of diagnosisCodes above.
+      let updatedDiagnoses = [];
+
+      for (let i = 0; i < diagnosisCodes.length; i++) {
+        const diagnosesWithDescription = filterPatientDiagnosisInfo(data, diagnosisCodes[i])
+        updatedDiagnoses.push(diagnosesWithDescription)
+      }
+
+      setPatientDiagnosisInfo(updatedDiagnoses)
+    }
+
+    getDiagnosesInfo()
+
+  }, [])
+
   return (
     <div>
       <p><b>{date}</b>  {description}</p>
       <ul>
-        {diagnosisCodes?.map((code, index) => <li key={index}>{code}</li>)}
+        {patientDiagnosisInfo?.map((codeInfo, index) => <li key={index}>{codeInfo.code}: {codeInfo.name}</li>)}
       </ul>
     </div>
   )
